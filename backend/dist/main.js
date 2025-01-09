034ae716b23159ed04e6cbfcfaad3eff2ecff59d
yahyamohmuedpro99 <yahyamohmuedpro99@gmail.com>
2025-01-09 20:18:13 +0200

    update

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -17,7 +17,7 @@ async function bootstrap() {
         }
     });
     app.enableCors({
-        origin: ['http://localhost:8080', process.env.FRONTEND_URL || '*'],
+        origin: '*',
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization'],
